22432e3de925ceeb843fe3b760d8de90d38a5c99
Bogdan <bogdan.pettrovic@gmail.com>
2025-10-15 18:25:26 +0200

    Initial commit

---
+++ client/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="h-screen w-screen flex justify-center items-center text-8xl font-bold">
+      <h1>Hello world</h1>
+    </div>
+  );
+}
---
+++ server/src/index.ts
@@ -0,0 +1,8 @@
+import app from './server.js';
+import 'dotenv/config';
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
