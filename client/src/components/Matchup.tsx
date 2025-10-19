0ac6a962b3586e8feffbf7f74797210df8c0bff1
Bogdan <bogdan.pettrovic@gmail.com>
2025-10-19 21:07:14 +0200

    Added animations in matchup component

--- client/src/components/Matchup.tsx
+++ client/src/components/Matchup.tsx
@@ -1,7 +1,12 @@
+"use client"
+
 import { format } from 'date-fns'
 import Match from '../types/match'
+import { useState } from 'react'
 
-const Matchup = ({ match }: { match: Match }) => { 
+const Matchup = ({ match }: { match: Match }) => {
+  const [active, setActive] = useState('')
+ 
   const formatedDate = (): string => {
     const date = match.date && format(new Date(match.date), 'dd.MM HH:mm');
     return date || ''
@@ -9,6 +14,28 @@ const Matchup = ({ match }: { match: Match }) => {
 
   return (
     <div className="relative w-full h-20 bg-gradient-to-l from-30% from-[#667EEA] to-[#9333EA] rounded-xl flex items-center">
+      {/* Team 1 */}
+      <div className="group flex items-center gap-3 relative w-1/2 h-full cursor-pointer duration-300" onClick={() => setActive(match.team1.name)}>
+        <img src={ match.team1.logo } className={`${active === match.team1.name ? 'size-[4.5rem]' : 'size-16 group-hover:size-[4.5rem]'} ml-4 drop-shadow-xl/70 duration-300`} />
+        <h2 className="hidden md:block text-3xl lg:text-4xl font-semibold">{ match.team1.name }</h2>
+      </div>
+
+      {/* Team 2 */}
+      <div 
+        className={
+          `${active === match.team2.name ? "w-[63%] " : "w-1/2 "}
+           group absolute flex flex-row-reverse items-center justify-start gap-3 bg-gradient-to-l from-[#F39C12] to-[#FFA500] top-0 right-0 rounded-r-xl h-full cursor-pointer duration-400`}
+        style={{
+          clipPath: active === match.team1.name ? `polygon(14% 0, 100% 0, 100% 100%, 10% 100%)` 
+                  : active === match.team2.name ? `polygon(10% 0, 100% 0, 100% 100%, 14% 100%)` : 'polygon(0 0, 100% 0, 100% 100%, 0% 100%)',
+        }}
+        onClick={() => setActive(match.team2.name)}
+      >
+        <img src={ match.team2.logo } className={`${active === match.team2.name ? 'size-[4.5rem]' : 'size-16 group-hover:size-[4.5rem]'} mr-4 drop-shadow-xl/70 duration-300`} />
+        <h2 className="hidden md:block text-3xl lg:text-4xl text-[#f9fafd] font-semibold ml-4 lg:ml-0">{ match.team2.name }</h2>
+      </div>
+
+      {/* Displays date or live */}
       <div className='absolute left-2 -bottom-7'>
         {
           match.live ? 
@@ -17,16 +44,8 @@ const Matchup = ({ match }: { match: Match }) => {
             <h3 className='font-semibold text-xl'>{ formatedDate() }</h3>
         }
       </div>
-      <div className="group flex items-center gap-3 relative w-1/2 h-full cursor-pointer duration-300">
-        <img src={ match.team1.logo } className="size-16 group-hover:size-[4.5rem] ml-4 drop-shadow-xl/70 duration-300" />
-        <h2 className="hidden md:block text-3xl lg:text-4xl font-semibold">{ match.team1.name }</h2>
-      </div>
-      <div 
-        className="group absolute flex flex-row-reverse items-center justify-start gap-3 bg-gradient-to-l from-[#F39C12] to-[#FFA500] top-0 right-0 rounded-r-xl h-full w-1/2 cursor-pointer duration-300"
-      >
-        <img src={ match.team2.logo } className="size-16 group-hover:size-[4.5rem] mr-4 drop-shadow-xl/70 duration-300" />
-        <h2 className="hidden md:block text-3xl lg:text-4xl text-[#f9fafd] font-semibold ml-4 lg:ml-0">{ match.team2.name }</h2>
-      </div>
+
+      {/* Displays match format */}
       <div className='absolute right-2 -bottom-7'>
         <h3 className='font-semibold text-xl capitalize'>{ match.format }</h3>
       </div>
