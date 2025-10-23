71b90e879e342fca28d7a5cb3fa34a8bfdaa7113
Bogdan <bogdan.pettrovic@gmail.com>
2025-10-23 16:47:53 +0200

    Improved matchup animations

--- client/src/components/Matchup.tsx
+++ client/src/components/Matchup.tsx
@@ -26,7 +26,12 @@ const Matchup: React.FC<MatchupProps> = ({ match, setPredictions }) => {
   return (
     <div className="relative w-full h-20 bg-gradient-to-l from-30% from-[#667EEA] to-[#9333EA] rounded-xl flex items-center">
       {/* Team 1 */}
-      <div className="group flex items-center gap-3 relative w-1/2 h-full cursor-pointer duration-300" onClick={() => handleChange(match.team1.name)}>
+      <div 
+        className={
+          `${active === match.team2.name && "opacity-50 hover:opacity-80 "} 
+          group flex items-center gap-3 relative w-1/2 h-full cursor-pointer duration-300`}
+        onClick={() => handleChange(match.team1.name)}
+      >
         <img src={ match.team1.logo } className={`${active === match.team1.name ? 'size-[4.5rem]' : 'size-16 group-hover:size-[4.5rem]'} ml-4 drop-shadow-xl/50 duration-300`} />
         <h2 className="hidden md:block text-3xl lg:text-4xl font-semibold">{ match.team1.name }</h2>
       </div>
@@ -34,7 +39,7 @@ const Matchup: React.FC<MatchupProps> = ({ match, setPredictions }) => {
       {/* Team 2 */}
       <div 
         className={
-          `${active === match.team2.name ? "w-[63%] " : "w-1/2 "}
+          `${active === match.team1.name ? "w-1/2 opacity-50 hover:opacity-80 " :  active === match.team2.name ? "w-[63%] " : "w-1/2 "}
            group absolute flex flex-row-reverse items-center justify-start gap-3 bg-gradient-to-l from-[#F39C12] to-[#FFA500] top-0 right-0 rounded-r-xl h-full cursor-pointer duration-400`}
         style={{
           clipPath: active === match.team1.name ? `polygon(14% 0, 100% 0, 100% 100%, 10% 100%)` 
