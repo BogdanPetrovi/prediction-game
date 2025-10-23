afb1a61ebf51c090b77c1d62ed5429fdbb655e56
Bogdan <bogdan.pettrovic@gmail.com>
2025-10-24 00:19:16 +0200

    Added emojies and icons

--- client/src/components/ui/HistoryCard.tsx
+++ client/src/components/ui/HistoryCard.tsx
@@ -10,9 +10,9 @@ const HistoryCard = ({ historyCardProps }: { historyCardProps: tournamentHistory
         <h2 className="text-4xl font-bold text-center mt-2 mb-5">{ historyCardProps.tournamentName }</h2>
       </div>
       <div className="flex flex-col w-full h-full text-primary">
-        <h3 className="bg-gradient-to-r from-yellow-400 to-yellow-500 animate-gradient-bg text-white text-5xl font-bold text-center py-10">1. { historyCardProps.firstPlace }</h3>
-        <h3 className="bg-[#C0C0C0] text-4xl font-bold text-center py-7">2. { historyCardProps.secondPlace }</h3>
-        <h3 className="bg-[#b08d57] text-4xl font-bold text-center py-7 rounded-b-lg">3. { historyCardProps.thirdPlace} </h3>
+        <h3 className="bg-gradient-to-r from-yellow-400 to-yellow-500 animate-gradient-bg text-white text-5xl font-bold text-center py-7">🏆 <br /> { historyCardProps.firstPlace }</h3>
+        <h3 className="bg-[#C0C0C0] text-4xl font-bold text-center py-7">🥈 { historyCardProps.secondPlace }</h3>
+        <h3 className="bg-[#b08d57] text-4xl font-bold text-center py-7 rounded-b-lg">🥉 { historyCardProps.thirdPlace} </h3>
       </div>
     </div>
   )
