75a8709b3252448b16e5650fe7d5ff66c373d89e
Bogdan Petrovic <bogdan.pettrovic@gmail.com>
2026-02-13 19:48:15 +0100

    Changed to fetch from axios in middleware

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -1,50 +1,52 @@
-import backend from "@/services/api/backend";
-import { AxiosError } from "axios";
 import { NextRequest, NextResponse } from "next/server";
 
 export default async function middleware(req: NextRequest) {
   const pathname = req.nextUrl.pathname;
-  const protectedRoutes = ["/play", "/leaderboard", "/my-predictions", "/history"]
-  const isRouteProtected = protectedRoutes.includes(pathname)
+  const protectedRoutes = ["/play", "/leaderboard", "/my-predictions", "/history"];
+  const isRouteProtected = protectedRoutes.includes(pathname);
 
   try {
-    const cookieHeader = req.headers.get('cookie')
+    const cookieHeader = req.headers.get("cookie");
 
-    const result = await backend.get('/auth/me', {
-      headers: {
-        'Cookie': cookieHeader
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/auth/me`,
+      {
+        method: "GET",
+        headers: {
+          Cookie: cookieHeader ?? "",
+        },
       }
-    })
-    
-    if(isRouteProtected) {
-      if(result.data.loggedIn)
-        return NextResponse.next()
+    );
 
-      return NextResponse.redirect(new URL('/login', req.url))
+    const data = await response.json();
+
+    if (isRouteProtected) {
+      if (data.loggedIn) return NextResponse.next();
+      return NextResponse.redirect(new URL("/login", req.url));
     }
 
-    if(result.data.loggedIn)
-      return NextResponse.redirect(new URL('/play', req.url))
+    if (data.loggedIn)
+      return NextResponse.redirect(new URL("/play", req.url));
 
-    if(pathname === '/')
-      return NextResponse.redirect(new URL('/login', req.url))
+    if (pathname === "/")
+      return NextResponse.redirect(new URL("/login", req.url));
 
-    return NextResponse.next()
+    return NextResponse.next();
   } catch (error) {
-    if(error instanceof AxiosError)
-      console.log(error.message)
+    console.log("Middleware error:", error);
 
-    if(isRouteProtected)
-      return NextResponse.redirect(new URL('/login', req.url))
+    if (isRouteProtected)
+      return NextResponse.redirect(new URL("/login", req.url));
 
-    if(pathname === '/')
-      return NextResponse.redirect(new URL('/login', req.url))
-    
-    return NextResponse.next()
+    if (pathname === "/")
+      return NextResponse.redirect(new URL("/login", req.url));
+
+    return NextResponse.next();
   }
 }
 
 export const config = {
-  // match all paths except nextjs internals and static files
-  matcher: ['/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',],
-}
+  matcher: [
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+  ],
+};
