1604dc1415857d7b1049a7e896aadd5002d46f0d
Bogdan Petrović <160347824+BogdanPetrovi@users.noreply.github.com>
2026-02-13 19:52:36 +0100

    NotFound page build error fix

--- client/src/app/not-found.tsx
+++ client/src/app/not-found.tsx
@@ -2,12 +2,20 @@ import Link from "next/link";
 import "./globals.css";
 
 export default function NotFound() {
-  return(
+  return (
     <div className="h-screen w-screen flex flex-col items-center justify-center gap-6">
-      <h2 className="text-5xl font-bold">Ooops... Looks like this page doesn't exists</h2>
-      <h3 className="text-4xl font-semibold text-white/50">Go back to&nbsp;
-        <Link href={'/login'} className="text-blue-400 italic underline cursor-pointer">login page</Link>
+      <h2 className="text-5xl font-bold">
+        Ooops... Looks like this page doesn&apos;t exist
+      </h2>
+      <h3 className="text-4xl font-semibold text-white/50">
+        Go back to&nbsp;
+        <Link
+          href="/login"
+          className="text-blue-400 italic underline cursor-pointer"
+        >
+          login page
+        </Link>
       </h3>
     </div>
-  )
+  );
 }
