f04f77b686acf3df2bd6c4c5d335c1b13241e77f
Bogdan Petrovic <bogdan.pettrovic@gmail.com>
2026-03-12 14:10:59 +0100

    Added custom app error handler class

--- server/src/controllers/adminController.ts
+++ server/src/controllers/adminController.ts
@@ -3,6 +3,7 @@ import redisClient from "../config/redis.js";
 import { HLTV } from "@bogdanpet/hltv";
 import database from "../database/database.js";
 import hltvWrapper from "../utils/hltvWrapper.js";
+import AppError from "../utils/customErrorHandlers/appError.js";
 
 export const addEvent = async (req: Request, res: Response) => {
   const { id, isActive, parentEventId } = req.body;
@@ -10,14 +11,14 @@ export const addEvent = async (req: Request, res: Response) => {
   console.log(`New event id:${id} \n Parent event id:${parentEventId} \n Is event active:${isActive}`)
 
   if(!id)
-    return res.status(400).send("You need to provide id!")
+    throw new AppError("You need to provide id!", 400)
 
   try {
     console.log('Checking HLTV for event... ' + new Date().toISOString())
     const event = await hltvWrapper(HLTV.getEvent({ id: parseInt(id) }))
 
     if(!event)
-      return res.status(400).send("This event doesn't exists! Double check the id")
+      throw new AppError("This event doesn't exists! Double check the id", 400)
 
     await database.query(`INSERT INTO events (id, name, logo, start_date, end_date, is_active, parent_event_id)
                           VALUES ($1, $2, $3, $4, $5, $6, $7)
--- server/src/utils/globalErrorHandler.ts
+++ server/src/utils/globalErrorHandler.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import { DatabaseError } from "pg";
 import { ErrorReply } from "redis";
+import AppError from "./customErrorHandlers/appError.js";
 
 export default function globalErrorHandler(
   err: unknown,
@@ -19,6 +20,10 @@ export default function globalErrorHandler(
   if(err instanceof TypeError){
     console.error(err.message)
     return res.status(500).send(err.message)
+  }
+  if(err instanceof AppError){
+    console.error(err.message)
+    return res.status(err.statusCode).send(err.message)
   }
   if(err instanceof Error){
     console.error(err.message)
