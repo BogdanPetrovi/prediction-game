6781d0578657618980f7cdfa59efeb35849a5493
Bogdan Petrovic <bogdan.pettrovic@gmail.com>
2026-05-19 15:43:56 +0200

    Added database check if redis doesnt have matches

--- server/src/controllers/matchController.ts
+++ server/src/controllers/matchController.ts
@@ -1,18 +1,46 @@
 import { Request, Response } from "express";
 import redisClient from "../config/redis.js";
-import { HLTV } from "@bogdanpet/hltv";
 import database from "../database/database.js";
-import hltvWrapper from "../utils/hltvWrapper.js";
 import matchesPoints from "../utils/matchesPoints.js";
-import TeamNames from "../types/TeamNames.js";
 
 export const getMatches = async (req: Request, res: Response) => {
   const matches = await redisClient.get("matches");
-
+  
   if(matches)
     return res.status(200).json(JSON.parse(matches))
 
-  return res.status(200).json([])
+ const result = await database.query(`
+    SELECT
+      id,
+      (team1).name AS team1_name,
+      (team1).logo AS team1_logo,
+      (team2).name AS team2_name,
+      (team2).logo AS team2_logo,
+      event_id,
+      date,
+      format
+    FROM matches
+    WHERE result IS NULL;
+  `)
+
+  const formatedMatches = result.rows.map((m) => ({
+    id: m.id,
+    team1: {
+      name: m.team1_name,
+      logo: m.team1_logo,
+    },
+    team2: {
+      name: m.team2_name,
+      logo: m.team2_logo,
+    },
+    event: {
+      id: m.event_id,
+    },
+    date: m.date,
+    format: m.format,
+  }))
+
+  return res.status(200).json(formatedMatches)
 }
 
 export const getMatchesPoints = async (req: Request, res: Response) => {
