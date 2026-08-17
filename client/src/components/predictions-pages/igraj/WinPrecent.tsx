c3a196eae204e905899329e84c6229d110346864
Bogdan Petrovic <bogdan.pettrovic@gmail.com>
2026-08-17 21:30:27 +0200

    Fixed a bug where opacity on win precent would change even if the match is live

--- client/src/components/predictions-pages/igraj/WinPrecent.tsx
+++ client/src/components/predictions-pages/igraj/WinPrecent.tsx
@@ -11,10 +11,21 @@ const WinPercent = ({ value, team, selected, isLive }: WinPrecentProps) => {
   return (
     <>
     { (selected !== '' || isLive) &&
-      <div className={`${team === 'team1' ?
-       'bg-white/20 slide-in-left' :
-       `bg-black/25 slide-in ${ selected === 'team1' && 'opacity-50 group-hover:opacity-80' }`} py-1 lg:py-1 px-0 lg:px-1 rounded-lg backdrop-blur-md font-semibold text-xs lg:text-lg duration-300`}
-       title="Broj poena koji osvajas ako ova ekipa pobedi">
+      <div 
+        className={`
+          ${ team === 'team1' ?
+            'bg-white/20 slide-in-left' 
+            :
+            `
+              bg-black/25 slide-in
+              ${selected === 'team1' && 'opacity-50'} 
+              ${!isLive && selected === 'team1' && 'group-hover:opacity-80'}
+            `
+          }
+          py-1 lg:py-1 px-0 lg:px-1 rounded-lg backdrop-blur-md font-semibold text-xs lg:text-lg duration-300
+        `}
+        title="Broj poena koji osvajas ako ova ekipa pobedi"
+      >
         {value}pts
       </div>
     }
