8b21d832b6a5504d5265c43a6866de101e6a0974
Bogdan Petrovic <bogdan.pettrovic@gmail.com>
2026-08-29 13:01:24 +0200

    Added no event handle in admin control table

--- client/src/components/admin/DashboardCard.tsx
+++ client/src/components/admin/DashboardCard.tsx
@@ -1,7 +1,14 @@
 import Image from "next/image"
 import { type Event } from '../../types/Event'
 
-const DashboardCard = ({ title, name, logo }: Event & { title: string }) => { 
+const DashboardCard = ({ title, name, logo }: Event & { title: string }) => {
+  if(!logo) return (
+    <div className="w-1/4 h-80 bg-secondary rounded-xl border p-3 gap-10 flex flex-col text-3xl">
+      <h2 className="font-bold">{ title }</h2>
+      <h3>Nema aktivnog turnira.</h3>
+    </div>
+  )
+
   return (
     <div className="w-1/4 h-80 bg-secondary rounded-xl border p-3 flex flex-col text-3xl">
       <h2 className="font-bold">{ title }</h2>
